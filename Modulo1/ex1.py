b56081d4eda021a443523065ea94f70c868da911
pfreisleben <pedrofreislebeen@gmail.com>
2021-05-11 19:21:53 -0300

    ok

---
+++ Modulo1/ex1.py
@@ -0,0 +1,16 @@
+# Declaração de variável
+def imprime_oi():
+    print("Olá Mundo!")
+
+
+imprime_oi()
+
+# Função com parâmetros
+
+
+def soma(x, y):
+    var = x + y
+    return var
+
+
+print(soma(10, 5))
