ca30bcf7f2993ec35a819ee044f4f01104504208
pfreisleben <pedrofreislebeen@gmail.com>
2021-05-11 20:07:27 -0300

    variavel e função

--- Modulo1/ex1.py
+++ Modulo1/ex1.py
@@ -1,16 +1,23 @@
-# Declaração de variável
-def imprime_oi():
-    print("Olá Mundo!")
-
-
-imprime_oi()
-
-# Função com parâmetros
+# Declaração de variável global e dentro do escopo da função
+global a
+a = 10
 
 
 def soma(x, y):
     var = x + y
+    print(a)
     return var
 
 
 print(soma(10, 5))
+
+###########################################################################
+# Função com valor padrão
+
+
+def aumento_salarial(salario=5000, percentual=10):
+    return salario*percentual/100 + salario
+
+
+salario_fulano = aumento_salarial(1000, 50)
+print(salario_fulano)
