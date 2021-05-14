6f5e9c3bd40339ab59304105c645f0982ff0255c
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-05-14 17:08:46 -0300

    ok

--- Modulo1/Aula 08 - Listas /Exercicio2.py
+++ Modulo1/Aula 08 - Listas /Exercicio2.py
@@ -2,3 +2,25 @@
 Faça um jogo da forca. O programa terá uma lista de palavras lidas de um arquivo
  texto e escolherá uma aleatoriamente. O jogador poderá errar 6 vezes antes de 
  ser enforcado."""
+
+palavra = input("Informe a palavra: ").lower()
+palavraOculta = ""
+chances = 0
+for letra in range(len(palavra)):
+    palavraOculta = palavraOculta + "_"
+for i in range(len(palavra)):
+    if i >= 6:
+        print("Você esgotou suas chances, cabô!")
+        break
+    else:
+        print(f'Você ainda tem {6-i} chances! ')
+        print("Palavra a ser advinhada: ")
+        print(palavraOculta)
+        palpite = input(
+            "Digite uma letra e tente completar a palavra: ").lower()
+        for index in range(len(palavra)):
+            if palavra[index] == palpite:
+                palavraOculta = list(palavraOculta)
+                palavraOculta[index] = palpite
+                string = ''.join(palavraOculta)
+                palavraOculta = string
