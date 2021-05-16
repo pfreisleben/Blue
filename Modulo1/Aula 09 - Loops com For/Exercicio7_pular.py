d24f3bbd00da44fe3329c397476204d0420f3fd0
pfreisleben <pedrofreislebeen@gmail.com>
2021-05-16 12:53:18 -0300

    ok

--- Modulo1/Aula 09 - Loops com For/Exercicio7_pular.py
+++ Modulo1/Aula 09 - Loops com For/Exercicio7_pular.py
@@ -4,3 +4,19 @@ impressa a quantidade de tentativas restantes e se o número aleatório é maior
  menor do que o número inserido na tentativa atual. Se o usuário não acertar em 
  10 tentativas, informe qual o número aleatório. [Dica: use a função randint 
  para gerar o número aleatório] """
+
+import random
+
+aleatorio = random.randint(1, 100)
+chances = 0
+while chances <= 10:
+    print(f'Quantidade de tentativas restantes: {10-chances}')
+    tentativa = int(input("Informe o número: "))
+    if aleatorio > tentativa:
+        print("O número aleatório é maior que a alternativa.")
+    elif aleatorio < tentativa:
+        print("O número aleatório é menor que a alternativa")
+    else:
+        print("Você acertou o número!")
+        break
+    chances += 1
