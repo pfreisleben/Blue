4d186186df03903848ace4845d44fe85ba9b1cca
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-05-17 16:47:01 -0300

    ok

--- Modulo1/Aula 09 - Loops com For/Exercicio5_pular.py
+++ Modulo1/Aula 09 - Loops com For/Exercicio5_pular.py
@@ -8,18 +8,26 @@ deve ser informado pelo operador para indicar o final da compra. O programa deve
  o programa deverá voltar ao ponto inicial, para registrar a próxima compra. 
  A saída deve ser conforme o exemplo abaixo:"""
 
-while True:
-    vlrProdutos = []
-    print("Olá, vamos iniciar o registro dos itens.")
-    for produto in range(1, 1000):
-        valor = float(
-            input(f'Digite o valor do produto {produto}, ou digite 0 para terminar. '))
-        if valor == 0:
+
+while True:  # Repetição sempre irá rodar, pois a condição será sempre True.
+    print("Bem-vindo, vamos iniciar o registro da compra! ")
+    lista1 = []  # Lista usada para receber o nome do produto
+    lista2 = []  # Lista para receber os preços inseridos
+    # Essa variável vai ser utilizada para checar se o programa deverá registrar novos itens ou não.
+    registraItem = True
+    while registraItem:  # Caso registraItem == True, rode:
+        produto = input("Informe o produto, ou digite 0 para terminar:")
+        if produto == "0":
+            print(f'Encerrando registro dos itens...')
+            # Faz a variavel registraItem ser False, para que não peça novos itens
+            registraItem = False
             break
         else:
-            vlrProdutos.append(valor)
-    print(f'Total da compra: R${sum(vlrProdutos)}')
-    dinheiro = float(
-        input("Informe o valor em dinheiro que o cliente forneceu: "))
-    print(f'Troco: {dinheiro-sum(vlrProdutos)}')
-    print("Iniciando nova compra...")
+            preco = float(input("Informe o valor que vai pagar em dinheiro: ").replace(
+                ',', '.'))  # Recebe preço do produto
+            lista1.append(produto)  # Insere o nome na lista.
+            lista2.append(preco)  # Insere o preço na lista.
+    # A função sum(list) faz a soma dos valores dentro da lista
+    print(f'Total da compra: {sum(lista2)}')
+    dinheiro = float(input("Digite o valor pago pelo cliente: "))
+    print(f'O troco que deverá ser dado: {dinheiro - sum(lista2)}')
