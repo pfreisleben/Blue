8fd6328c7eba6b357895dde83e014c190dd2a89d
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-05-17 09:31:56 -0300

    ok

--- Modulo1/Aula 10  - While/Exercicio 4.py
+++ Modulo1/Aula 10  - While/Exercicio 4.py
@@ -3,3 +3,10 @@ palavra digitada. """
 palavra = input("Digite a palavra do jogo")
 palavraOculta = ""
 tentativas = []
+for letra in palavra:
+    palavraOculta += "_ "
+
+while "_" in palavraOculta:
+print(f' Palavra a ser acertada: {palavraOculta} ')
+palpite = input("Digite uma letra para tentar completar a palavra: ")
+while palpite in tentativas:
