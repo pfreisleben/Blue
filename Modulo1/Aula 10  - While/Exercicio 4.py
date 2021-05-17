749b542147eaff34ad23527caa4de338485e5e2c
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-05-17 09:32:24 -0300

    ok

--- Modulo1/Aula 10  - While/Exercicio 4.py
+++ Modulo1/Aula 10  - While/Exercicio 4.py
@@ -7,6 +7,6 @@ for letra in palavra:
     palavraOculta += "_ "
 
 while "_" in palavraOculta:
-print(f' Palavra a ser acertada: {palavraOculta} ')
-palpite = input("Digite uma letra para tentar completar a palavra: ")
-while palpite in tentativas:
+    print(f' Palavra a ser acertada: {palavraOculta} ')
+    palpite = input("Digite uma letra para tentar completar a palavra: ")
+# while palpite in tentativas:
