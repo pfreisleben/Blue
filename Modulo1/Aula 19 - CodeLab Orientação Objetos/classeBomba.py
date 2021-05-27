8b2c193a533ef74b79aa4bc88b31b56983c4331d
pfreisleben <pedrofreislebeen@gmail.com>
2021-05-27 20:24:00 -0300

    ok

--- Modulo1/Aula 19 - CodeLab Orientação Objetos/classeBomba.py
+++ Modulo1/Aula 19 - CodeLab Orientação Objetos/classeBomba.py
@@ -9,30 +9,40 @@ class bombaCombustivel:
         self.quantidadeCombustivel = quantidadeCombustivel
 
     def __str__(self):
-        return f'Tipo de combustivel atual: {self.tipoCombustivel}, valor atual: {self.valorLitro}, quantidade atual: {self.quantidadeCombustivel}'
+        return f'Tipo de combustivel atual: {self.tipoCombustivel}, valor atual: {self.valorLitro}, quantidade atual: {self.quantidadeCombustivel:.2f}'
 
-    def abastecerPorValor(self, valor):
+    def abastecerPorValor(self):
+        valor = float(input("Digite o valor a ser abastecido: "))
         qtdLitros = valor / self.valorLitro
         self.quantidadeCombustivel -= qtdLitros
+        self.abastecer(qtdLitros)
         return qtdLitros
 
-    def abastecerPorLitro(self, litro):
+    def abastecerPorLitro(self):
+        litro = float(
+            input("Digite a quantidade em litros a ser abastecida: "))
         vlrAbastecimento = litro * self.valorLitro
         self.quantidadeCombustivel -= litro
+        self.abastecer(litro)
         return vlrAbastecimento
 
-    def alterarValor(self, novoValor):
+    def alterarValor(self):
+        novoValor = float(input("Informe o novo valor por litro: "))
         self.valorLitro = float(novoValor)
 
-    def alterarCombustivel(self, novoCombustivel):
+    def alterarCombustivel(self):
+        novoCombustivel = input("Informe o novo combustivel: ")
         self.tipoCombustivel = novoCombustivel
 
-    def alterarQuantidadeCombustivel(self, novaQuantidade):
+    def alterarQuantidadeCombustivel(self):
+        novaQuantidade = float(
+            input("Digite a nova quantidade disponivel de combustivel: "))
         self.quantidadeCombustivel = novaQuantidade
 
     def abastecer(self, litros):
-        qtdAbastecida = 0
-        for i in range(litros):
-            print(f'Abastecendo.... {qtdAbastecida} litros.')
-            qtdAbastecida += 1
+        litrosInteiro = int(litros)
+        sobra = litros - litrosInteiro
+        for i in range(1, int(litros)+1):
+            print(f'Abastecendo.... {i} litro.')
             time.sleep(0.5)
+        print(f'Abastecendo... {sobra:.2f} litro.')
--- Modulo1/Aula 19 - CodeLab Orientação Objetos/programa.py
+++ Modulo1/Aula 19 - CodeLab Orientação Objetos/programa.py
@@ -1,5 +1,28 @@
 from classeBomba import bombaCombustivel
 
-bomba = bombaCombustivel("etanol", 3, 500)
+tipo = input("Digite o tipo de combustivel: ")
+valor = float(input("Digite o valor por litro do combustivel: "))
+qtdLitros = float(
+    input("Informe a quantidade de litros disponivel no posto: "))
+bomba = bombaCombustivel(tipo, valor, qtdLitros)
 
-print(bomba)
+while True:
+    print(bomba)
+    print(f'1 - Abastecer por valor')
+    print(f'2 - Abastecer por litro')
+    print(f'3 - Alterar valor do litro do combustivel')
+    print(f'4 - Alterar o tipo de combustivel')
+    print(f'5 - Alterar quantidade disponivel de combustivel')
+    opcao = input("Digite sua opção: ")
+    if opcao == '1':
+        bomba.abastecerPorValor()
+    elif opcao == '2':
+        bomba.abastecerPorLitro()
+    elif opcao == '3':
+        bomba.alterarValor()
+    elif opcao == '4':
+        bomba.alterarCombustivel()
+    elif opcao == '5':
+        bomba.alterarQuantidadeCombustivel()
+    else:
+        print(f'Opção inválida!')
