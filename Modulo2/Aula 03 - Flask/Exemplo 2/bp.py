704493a45ad0af7f9e12d72047a3ff6d719eb776
pfreisleben <pedrofreislebeen@gmail.com>
2021-06-11 00:12:16 -0300

    ok

--- Modulo2/Aula 03 - Flask/Exemplo 2/bp.py
+++ Modulo2/Aula 03 - Flask/Exemplo 2/bp.py
@@ -5,7 +5,12 @@ bp = Blueprint('bp', __name__)
 @bp.route("/")
 def index():
     nome = "Marcio"
-    idade = 25
+    idade = 17
     cidade = "São Paulo"
     imagem = "/static/logo-blue-croped.gif"
-    return render_template('index.html', nome=nome, idade=idade, cidade=cidade, imagem=imagem)
+    if idade >= 18:
+        maiordeidade = True
+    else:
+        maiordeidade = False
+    return render_template('index.html', nome=nome, idade=idade, cidade=cidade, imagem=imagem, maiordeidade=maiordeidade
+                           )
