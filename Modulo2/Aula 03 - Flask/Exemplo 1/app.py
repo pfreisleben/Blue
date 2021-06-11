1b27004ab75ce0dad31cfabb5993b0239369fbd9
pfreisleben <pedrofreislebeen@gmail.com>
2021-06-10 21:18:17 -0300

    ok

--- Modulo2/Aula 03 - Flask/Exemplo 1/app.py
+++ Modulo2/Aula 03 - Flask/Exemplo 1/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return"<h1>Hello World!</h1>"
+    return"<h1>Hello World!!</h1>"
+
+
+@app.route("/login/<nome>")
+def login(nome=None):
+    return "<center><h1>Olá, "+nome+"<br/> Faca seu login!</h1></center>"
+
+
+@app.route("/auladeontem/")
+def carrega():
+    return render_template("inicio.html")
+
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
