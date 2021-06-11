99079465969cc664d47e8ea31e9b37a26cedd18e
pfreisleben <pedrofreislebeen@gmail.com>
2021-06-10 22:02:02 -0300

    ok

--- Modulo2/Aula 03 - Flask/Exemplo 1/app.py
+++ Modulo2/Aula 03 - Flask/Exemplo 1/app.py
@@ -1,20 +1,7 @@
+from bp import bp
 from flask import Flask, render_template
 app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():
-    return"<h1>Hello World!!</h1>"
-
-
-@app.route("/login/<nome>")
-def login(nome=None):
-    return "<center><h1>Olá, "+nome+"<br/> Faca seu login!</h1></center>"
-
-
-@app.route("/auladeontem/")
-def carrega():
-    return render_template("inicio.html")
+app.register_blueprint(bp)
 
 
 if(__name__ == "__main__"):
