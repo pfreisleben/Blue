245208c0486c7e921f64f697235a2c9a496744da
pfreisleben <pedrofreislebeen@gmail.com>
2021-06-14 22:04:56 -0300

    ok

--- Modulo2/Aula 06 - Forms/Exemplo 1/bp.py
+++ Modulo2/Aula 06 - Forms/Exemplo 1/bp.py
@@ -4,15 +4,5 @@ bp = Blueprint('bp', __name__)
 
 
 @bp.route("/")
-def hello_world():
-    return"<h1>Hello World!!</h1>"
-
-
-@bp.route("/login/<nome>")
-def login(nome=None):
-    return "<center><h1>Olá, "+nome+"<br/> Faca seu login!</h1></center>"
-
-
-@bp.route("/auladeontem/")
-def carrega():
-    return render_template("inicio.html")
+def index():
+    return render_template('index.html')
