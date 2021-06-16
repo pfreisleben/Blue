45db6a174ab1cfc3ccd43a233d8982c3e170e887
pfreisleben <pedrofreislebeen@gmail.com>
2021-06-16 19:35:01 -0300

    ok

--- Modulo2/Aula 07 - JS/Exemplo 01 - Inline/scripts.js
+++ Modulo2/Aula 07 - JS/Exemplo 01 - Inline/scripts.js
@@ -1,3 +1,10 @@
-function mensagem() {
-  alert('Hello World!');
+function altera() {
+  const elemento1 = document.getElementById('botao');
+  if (elemento1.innerText == 'Texto mudou') {
+    elemento1.innerText = 'Texto voltou';
+    document.bgColor = 'black';
+  } else {
+    elemento1.innerText = 'Texto mudou';
+    document.bgColor = 'red';
+  }
 }
--- Modulo2/Aula 07 - JS/Teste/app.py
+++ Modulo2/Aula 07 - JS/Teste/app.py
@@ -1,6 +1,8 @@
-from Flas import Flask
+from flask import Flask  # Importa flask
+from routes import routes  # Importa o Blueprint "routes" criado em ./routes.py
 
-app = Flask(__name__)
+app = Flask(__name__)  # Instancia o Flask
+app.register_blueprint(routes)
 
 if (__name__ == "__main__"):
     app.run(Debug=True)
