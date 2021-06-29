9ea9b320640bbb6bf147bbcdf2bb176e8d8d9b58
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-06-29 13:08:54 -0300

    ok

---
+++ Modulo2/Aula 16 - CRUD Flask/Projeto - Aula 16/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Configurações de acesso ao banco de dados
+user = 'bzglohyf'
+password = 'tiVqxFosBTmmN6P3wsMfdRM7_4EqFPmN'
+host = 'tuffi.db.elephantsql.com'
+database = 'bzglohyf'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{database}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "uma chave secreta bem secreta"
+
+# Instanciando objeto da classe SQLAlchemy
+db = SQLAlchemy(app)
+
+# Modelar a Classe Filmes -> tabela filmes
+class Filmes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(255), nullable=False)
+    imagem_url = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, nome, imagem_url):
+        self.nome = nome
+        self.imagem_url = imagem_url
+    
+    @staticmethod
+    def read_all():
+        # SELECT * FROM filmes ORDER BY id ASC
+        return Filmes.query.order_by(Filmes.id.asc()).all()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/read")
+def read_all():
+    # Chamada do método read_all da classe Filmes, que representa a tabela filmes do banco de dados.
+    registros = Filmes.read_all()
+    return render_template("read_all.html", registros=registros)
+
+
+@app.route("/read/<id_registro>")
+def read_id(id_registro):
+    return "Em construção - Visualizar registro de ID "+id_registro
+
+
+@app.route("/create")
+def create():
+    return "Em construção - Ainda será feito o CREATE!"
+
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
