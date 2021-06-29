21301785bc7c7a89317303ec8426c13bbf0623bc
pfreisleben <pedrofreislebeen@gmail.com>
2021-06-29 20:48:22 -0300

    ok

--- Modulo2/Aula 16 - CRUD Flask/Meu Projeto - Aula 16/app.py
+++ Modulo2/Aula 16 - CRUD Flask/Meu Projeto - Aula 16/app.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# Configurações do banco de dados
+user = 'dwxnfeeu'  # Usuário do banco
+database = 'dwxnfeeu'  # Nome do banco
+host = 'tuffi.db.elephantsql.com'  # Host do banco
+password = 'EYsro5jAqVKp9hnoUgX_G8Tw3IjMd9Jo'  # Senha do banco
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{database}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "uma chave secreta bem secreta"
+
+# Cria o objeto SQL Alchemy, passando o app como parametro e vinculando a variavel db.
+db = SQLAlchemy(app)
 
 
-@app.route("/")
+class Filmes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(255), nullable=False)
+    imagem_url = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, nome, imagem_url):
+        self.nome = nome
+        self.imagem_url = imagem_url
+
+    @staticmethod
+    def read_all():
+        return Filmes.query.all()
+
+
+@app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/read')
-def read_all():
-    return render_template('read_all.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
